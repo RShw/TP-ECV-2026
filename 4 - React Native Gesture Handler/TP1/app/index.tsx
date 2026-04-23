1e7b6d47c74a90a28bf103dc4d09a81bbf168aeb
LudivinDivin <augustcasse@gmail.com>
2026-04-23 14:27:26 +0200

    End Of TP1 of course 1

--- 4 - React Native Gesture Handler/TP1/app/index.tsx
+++ 4 - React Native Gesture Handler/TP1/app/index.tsx
@@ -1,15 +1,69 @@
-import { Text, View } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Gesture, GestureDetector, GestureHandlerRootView } from "react-native-gesture-handler";
+import Animated, { Extrapolation, interpolate, useAnimatedStyle, useSharedValue, withSpring } from "react-native-reanimated";
 
 export default function Index() {
+
+  const positionX = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+
+    const rotate = interpolate(positionX.value, [-400, 0, 400], [-10, 0, 10], Extrapolation.CLAMP);
+
+    return {
+      transform: [
+        { 
+        translateX: positionX.value,
+        },
+        {
+          scale: interpolate(positionX.value, [-400, 0, 400], [0.6, 1, 0.6], Extrapolation.CLAMP)
+        },
+        {
+          rotate: rotate + "deg"
+        }
+      ]
+    }
+  });
+
+  const gesture = Gesture.Pan()
+    .onUpdate((e) => {
+      console.log(e.translationX);
+      positionX.value = e.translationX;
+    })
+    .onEnd(() => {
+      positionX.value = withSpring(0);
+    })
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <GestureHandlerRootView>
+      <View
+        style={styles.container}
+      >
+        <GestureDetector gesture={gesture}>
+          <Animated.View style={[styles.cardContainer, animatedStyle]} >
+            <View style={styles.card} />
+          </Animated.View>
+        </GestureDetector>
+      </View>
+    </GestureHandlerRootView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "flex-end",
+  },
+  cardContainer:  {
+    height: 400,
+    backgroundColor: "salmon",
+    borderRadius: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+  },
+  card: {
+    flex: 1,
+    borderRadius: 12,
+    marginHorizontal: 12,
+  }
+});
